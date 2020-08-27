fb3bdafe454a2fceb743d0f14c786afb21d0a50c
David <cosi@outlook.com>
2020-08-26 23:26:39 -0500

    Update Contact Form

---
+++ js/dist/contact.form.min.dev.js
@@ -0,0 +1,51 @@
+"use strict";
+
+jQuery(document).ready(function (e) {
+  "use strict";
+
+  e(function () {
+    e("#contact").validate({
+      rules: {
+        name: {
+          required: !0,
+          minlength: 2
+        },
+        surname: {
+          required: !0,
+          minlength: 2
+        },
+        email: {
+          required: !0,
+          email: !0
+        },
+        subject: {
+          required: !0
+        },
+        message: {
+          required: !0
+        }
+      },
+      messages: {
+        name: {
+          required: "Please type your name",
+          minlength: "Please type your name correctly"
+        },
+        surname: {
+          required: "Please type your surname",
+          minlength: "Please type your surname correctly"
+        },
+        email: {
+          required: "Please type your e-mail correctly"
+        },
+        subject: {
+          required: "Please type your number",
+          minlength: "To short number"
+        },
+        message: {
+          required: "Please type your message",
+          minlength: "To short message"
+        }
+      }
+    });
+  });
+});
