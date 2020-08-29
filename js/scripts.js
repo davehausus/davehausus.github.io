0348ae8faf60adeac5476c0ac44c58e9f40980e4
David <cosi@outlook.com>
2020-08-28 21:37:31 -0500

    Video Play Button

--- js/scripts.js
+++ js/scripts.js
@@ -335,7 +335,18 @@
 			);
 			wow.init();
 	
-	
+		
+		// VIDEO OVERLAY 
+			$('.video').parent().click(function () {
+				if($(this).children(".video").get(0).paused){        $(this).children(".video").get(0).play();   $(this).children(".playpause").fadeOut();
+				  }else{       $(this).children(".video").get(0).pause();
+				$(this).children(".playpause").fadeIn();
+				  }
+			});
+		
+		
+
+
 	
 		// COUNTER
 			 $(document).scroll(function(){
