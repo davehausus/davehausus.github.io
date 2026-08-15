b51e79bdecf05dc53017f4d4513ab86a9296b219
David <cosi@outlook.com>
2026-08-14 23:02:04 -0600

    New Nefis Sitemap

--- sites/nefis/astro.config.mjs
+++ sites/nefis/astro.config.mjs
@@ -1,28 +1,40 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightRosePine from 'starlight-theme-rose-pine'
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://daviddejesus.me',
-	base: '/nefis',
-	integrations: [
+  site: 'https://daviddejesus.me',
+  base: '/nefis',
+
+  integrations: [
     	starlight({
+        	plugins: [starlightRosePine()],
       		title: 'Nefis Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
+    			customCss: [
+			    	'./src/styles/global.css',
+      			],
+        	social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+        	sidebar: [
+            	{
+                  label: 'Guides',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'Example Guide', slug: 'guides/example' },
+                  ],
+              },
+              {
+                  label: 'Reference',
+                  items: [{ autogenerate: { directory: 'reference' } }],
+              },
+          ],
+      }),
 	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
--- sites/nomo/astro.config.mjs
+++ sites/nomo/astro.config.mjs
@@ -1,28 +1,40 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeVintage from 'starlight-theme-vintage'
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://daviddejesus.me',
-	base: '/nomo',
-	integrations: [
+  site: 'https://daviddejesus.me',
+  base: '/nomo',
+
+  integrations: [
     	starlight({
+        	plugins: [starlightThemeVintage()],
       		title: 'Nomo Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
+			    customCss: [
+        		'./src/styles/global.css',
+			      ],
+    		  social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+        	sidebar: [
+            	{
+                  label: 'Guides',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'Example Guide', slug: 'guides/example' },
+                  ],
+              },
+              {
+                  label: 'Reference',
+                  items: [{ autogenerate: { directory: 'reference' } }],
+              },
+          ],
+      }),
 	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
